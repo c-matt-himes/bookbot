9346c0f36f9a9815f5104605dbd28ba73dde1d86
c_matt_himes <c.matt.himes@gmail.com>
2025-03-22 11:46:33 -0400

    add notes to main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ path = "" #define path as a string
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+#takes a filepath and returns full contents of the book
 
 def main():
     path = "./books/frankenstein.txt"
     print(get_book_text(path))
+#enters filepath as string path, prints the full contents
 
 main()
 
